35e6f78160e00279fe2c67b71a3a1d94480ead52
Esteban Patiño <esteban.pg45@gmail.com>
2023-09-20 00:56:04 -0500

    Initial Structure

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+const app = require('./app')
+
+
+const connection_string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`
+
+mongoose.connect(connection_string, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(()=> {
+            app.listen(process.env.PORT, () => console.log(`IP SERVER:\nhttp://${process.env.IP_SERVER}:${process.env.PORT}${process.env.API}${process.env.API_VERSION}`))
+        })
