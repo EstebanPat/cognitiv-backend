ae356ec3b16833b78dd82f8fcc66294f749b9593
Esteban Patiño <esteban.pg45@gmail.com>
2023-11-14 14:38:02 -0500

    Send Email to confirm the register of an user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const userRoutes = require('./routes/user')
 const membershipRoutes = require('./routes/membership')
 const suscriptionRoutes = require('./routes/suscription')
+const nonImTrainingRoutes = require('./routes/nonImTraining')
 
 require('dotenv').config()
 
@@ -22,7 +23,8 @@ app.use(function(req, res, next) {
 
 app.use(`${process.env.API}${process.env.API_VERSION}users`, userRoutes)
 app.use(`${process.env.API}${process.env.API_VERSION}memberships`, membershipRoutes)
-app.use(`${process.env.API}${process.env.API_VERSION}suscriptions`, suscriptionRoutes   )
+app.use(`${process.env.API}${process.env.API_VERSION}suscriptions`, suscriptionRoutes)
+app.use(`${process.env.API}${process.env.API_VERSION}nonimtrainings`, nonImTrainingRoutes)
 
 
 module.exports = app
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 require('dotenv').config();
 const app = require('./app')
 
-
 const connection_string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`
 
 mongoose.connect(connection_string, {
