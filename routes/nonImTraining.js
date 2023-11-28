04e5668239622b2851615a4ae9154e472d1fc0ab
Esteban Patiño <esteban.pg45@gmail.com>
2023-11-28 00:43:11 -0500

    Actualization in trainings and check the routine as finished

--- routes/nonImTraining.js
+++ routes/nonImTraining.js
@@ -5,6 +5,7 @@ const trainingController = require('../controllers/nonImTraining')
 
 router.post("/new-nonimtraining", trainingController.createTraining);
 router.get("/", trainingController.getAllTrainings);
+router.get("/:trainingId", trainingController.getTrainingById);
 router.delete("/delete/:trainingId", trainingController.deleteTraining);
 
 module.exports = router
--- routes/routine.js
+++ routes/routine.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const routineController = require("../controllers/routine")
 
-router.get("/:routineId", routineController.getRoutineById)
+router.get("/:routineId", routineController.getRoutineById);
+router.patch("/finish/:routineId", routineController.finishRoutine);
 
 module.exports = router
